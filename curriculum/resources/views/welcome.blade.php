dd4ae460f8c62c1b444550c0021c52fa4be9235b
Armando Ferlanti <armandoferlanti@outlook.it>
2023-11-25 12:42:11 +0100

    piccole modifiche grafiche iniziali

--- curriculum/resources/views/welcome.blade.php
+++ curriculum/resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
-  <h1 class="text-3xl font-bold text-center text-red-600">
-    Hello world!
+<body class="bg-slate-900">
+  <h1 class="text-3xl font-bold text-center text-orange-500">
+   ATTENZIONE: SITO IN COSTRUZIONE
   </h1>
 </body>
 </html>
