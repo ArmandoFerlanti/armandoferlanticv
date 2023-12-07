09df3ec90eb38e013a4cfd942cd3754984ecc654
Armando Ferlanti <armandoferlanti@outlook.it>
2023-12-07 10:21:40 +0100

    modificata animazione competenze lato web developer

--- curriculum/resources/views/components/competenze.blade.php
+++ curriculum/resources/views/components/competenze.blade.php
@@ -1,28 +1,51 @@
-<div id="contenitore-sezione-competenze" class="w-full h-auto flex items-center justify-center flex-col bg-white"> 
-                
-                
-                
-    <div id="titolo-competenze" class="mt-10 mb-5 bg-white font-thin-600">
-      FRAMEWORKS E LINGUAGGI CONOSCIUTI
-    </div>
-  
-             <div id="contenitore-competenze" class="bg-white w-5/6 h-auto flex-col flex items-center justify-center">
-
-                  <div class="m-2"> <img src="{{asset('immagini/html.webp')}}" alt="html" class="w-14 h-14"></div>
-                  <div class="m-2"> <img src="{{asset('immagini/css.png')}}" alt="css" class="w-14 h-14"></div>
-                  <div class="m-2"> <img src="{{asset('immagini/bootstrap.jpeg')}}" alt="bootstrap" class="w-14 h-14"></div>
-                 <div class="m-2"> <img src="{{asset('immagini/icona-javascript.ico')}}" alt="javascript" class="w-14 h-14"></div>
-                  <div class="m-2">  <img src="{{asset('immagini/php.png')}}" alt="php" class="w-14 h-14"></div>
-                <div class="m-2"> <img src="{{asset('immagini/laravel.png')}}" alt="laravel" class="w-14 h-14"></div>
-                 <div class="m-2"> <img src="{{asset('immagini/tailwind.webp')}}" alt="tailwind" class="w-14 h-14"></div>
-                 
 
 
-             </div>
+<div id="contenitore-sezione-competenze" class="w-full h-auto bg-white flex flex-col justify-center items-center ">
+
+  <div id="titolo-competenze" class="text-center my-5">LINGUAGGI E FRAMEWORKS CONOSCIUTI</div>
+
+              <div id="contenitore-competenze" class="w-4/5 h-auto bg-white flex flex-row justify-center">
+
+                        <div id="contenitore-icone" class="bg-white animate-slide-in">
+                            <div class="m-2 "> <img src="{{asset('immagini/html.webp')}}" alt="html" class="w-14 h-14"></div>
+                            <div class="m-2"> <img src="{{asset('immagini/css.png')}}" alt="css" class="w-14 h-14"></div>
+                            <div class="m-2"> <img src="{{asset('immagini/bootstrap.jpeg')}}" alt="bootstrap" class="w-14 h-14"></div>
+                            <div class="m-2"> <img src="{{asset('immagini/icona-javascript.ico')}}" alt="javascript" class="w-14 h-14"></div>
+                             <div class="m-2">  <img src="{{asset('immagini/php.png')}}" alt="php" class="w-14 h-14"></div>
+                            <div class="m-2"> <img src="{{asset('immagini/laravel.png')}}" alt="laravel" class="w-14 h-14"></div>
+                            <div class="m-2"> <img src="{{asset('immagini/tailwind.webp')}}" alt="tailwind" class="w-14 h-14"></div>
+                         
+                        </div>
+        
+                                <div id="contenitore-titoli" class="bg-white font-mono text-blue-600">
+
+                                  <div class="mt-6 ml-2 w-14 h-14">html</div>
+                                  <div class="mt-2 ml-2 w-14 h-14">css</div>
+                                  <div class="mt-2 ml-2 w-14 h-14">bootstrap</div>
+                                  <div class="m-2 w-14 h-14">javascript</div>
+                                  <div class="m-2 w-14 h-14">php</div>
+                                  <div class="m-2 w-14 h-14">laravel</div>
+                                  <div class="m-2 w-14 h-14">tailwind</div>
 
 
 
+                                </div>
+        
+                        
+        
+              </div>
 
 
 
 </div>
+
+
+
+
+
+
+
+
+
+
+
