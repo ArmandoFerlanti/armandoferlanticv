e4e873bd0e88babc55b1f25001733509df2dc59a
Armando Ferlanti <armandoferlanti@outlook.it>
2023-12-08 10:21:45 +0100

    ho creato una sezione certificazioni per il curriculum da sviluppatore

---
+++ curriculum/resources/views/components/certificazioni1.blade.php
@@ -0,0 +1,61 @@
+
+<div id="container-sezione-certificazioni" class="w-full h-auto mt-10">
+
+    <div id="titolo-certificazioni" class="text-center font-thin text-xl p-5">
+            CERTIFICAZIONI E COMPETENZE
+    </div>
+
+        <div id="certificazione" class="w-4/6  m-auto text-center h-auto">
+              <div class="mt-10">
+                  <img src="{{asset('immagini/Aulab.png')}}" alt="Aulab">
+              </div>
+
+                      <button class="mt-5 bg-blue-600 hover:bg-blue-800 rounded-full p-3 font-mono text-white">
+                        <a href="https://www.credential.net/dd048a8d-208a-4e4e-901a-f31e331e33e8#gs.1bbw47">mostra credenziale </a>
+                      </button>
+
+        </div>
+
+
+                <div id="contenitore-competenzee" class="w-5/6 h-auto text-justify p-5 m-auto font-extralight text-xl">
+                 
+                      
+                  <div class="m-auto p-5">
+                    Questa esperienza mi ha permesso di acquisire le seguenti competenze:
+                  </div>
+                  <div>
+                    <ul class="list-disc p-2">
+                      <li>Conoscenza di HTML5 e su come strutturare un sito web.</li>
+                      <li>Conoscenza di CSS e su come utilizzare le sue classi e i suoi selettori per rendere graficamente più accattivante un sito.</li>
+                      <li>Ho acquisito la conoscenza per implementare e utilizzare il framework Bootstrap nei miei progetti.</li>
+                      <li>Tramite JavaScript, ho acquisito la capacità di dare dinamicità e interattività a un sito, grazie alla manipolazione del DOM e alla gestione degli eventi.</li>
+                      <li>Approfondito la sintassi di PHP e la programmazione ad oggetti.</li>
+                      <li>Acquisito conoscenze pratiche su Laravel e su come utilizzarlo come framework full stack, sfruttando la sua flessibilità e il suo approccio MVC.</li>
+                      <li>Ho acquisito la capacità di utilizzare Git per campionare un progetto e per lavorare in team anche da remoto.</li>
+                      <li>Ho acquisito la conoscenza dei metodi di programmazione agili e sui benefici che apportano se si sceglie di utilizzarli.</li>
+                    </ul>
+                  </div>
+                      
+
+
+
+
+
+
+                </div>
+
+
+
+
+
+
+
+</div>
+
+          
+
+
+
+
+
+
--- curriculum/resources/views/welcome.blade.php
+++ curriculum/resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
   <x-presentazione1/>
   <x-competenze/>
   <x-attitudini/>
+  <x-certificazioni1/>
 
   <div>
     <h1 class="text-3xl font-serif text-center animate-pulse mt-5 text-orange-500 bg-black">
