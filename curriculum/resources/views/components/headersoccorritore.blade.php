d5afcc61846fd643312570dc4baac875e3006191
Armando Ferlanti <armandoferlanti@outlook.it>
2023-12-13 22:08:26 +0100

    ho risolto un problema con la visualizzazione della navbar sugli schermi grandi

--- curriculum/resources/views/components/headersoccorritore.blade.php
+++ curriculum/resources/views/components/headersoccorritore.blade.php
@@ -1,32 +1,18 @@
 
 
 
-<div id="header" class="bg-white w-full h-40 sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full flex items-center gap-6 sm:gap-36 md:gap-40 lg:gap-96 border-red-600 border-b-4">
+<div id="header" class=" w-full h-auto  flex  border-red-600 border-b-4">
+  <div id="foto-profilo" class="bg-black w-24 h-24 mt-4 ml-5 rounded-full">
+      <img src="{{asset('immagini/foto.jpg')}}" alt="foto-profilo" class="w-full h-full object-cover rounded-full">
+  </div>
 
-    <div id="foto-profilo" class="bg-black w-24 h-24 mt-4 ml-5 rounded-full xl:w-32 xl:h-32 md:w-28 md:h-28">
-        <img src="{{asset('immagini/foto.jpg')}}" alt="foto-profilo" class="w-full h-full object-cover rounded-full">
-   </div>
-
-
-    <div id="logo" class="bg-black w-44 h-32 ml-5 mt-4 xl:w-80 xl:ml-8 md:w-72 md:h-42 md:mr-0  ">
-      <img src="{{asset('immagini/logo-soccorritore.png')}}" alt="logo" class="w-full h-full object-cover">
-    </div>
-
-
-
-    
-
-
-
-
-
-
-
+  <div id="logo" class="bg-black  m-auto">
+     <img src="{{asset('immagini/logo-soccorritore.png')}}" alt="logo" class=" w-56 h-32 m-auto ">
+  </div>
 </div>
 
-              <div id="contenitore-intestazione" class="bg-red-600 w-full h-48 flex-col flex items-center justify-center ">
-          
-                 <div id="intestazione" class="bg-red-600 w-5/6  h-20 text-center m-auto flex items-center justify-center text-2xl font-extrabold text-white animate-slide-in">
-                          RESPONSABILE OPERAZIONI DI SOCCORSO IN QUOTA E SPAZI CONFINATI
-                  </div>
-              </div>
+<div id="contenitore-intestazione" class="bg-red-600 w-full h-48 flex-col flex items-center justify-center">
+  <div id="intestazione" class="bg-red-600 w-5/6 h-20 text-center m-auto flex items-center justify-center text-2xl font-extrabold text-white animate-slide-in">
+      RESPONSABILE OPERAZIONI DI SOCCORSO IN QUOTA E SPAZI CONFINATI
+  </div>
+</div>
